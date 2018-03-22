8ba9ef49d7686538ead1e9f87dd170c457c6956d
James Kim <jhlink@users.noreply.github.com>
2018-03-22 13:20:39 -0400

    task: CP TK [15, 17], create ForecastAdapter and ForecastAdapterViewHolder classes

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by james on 3/22/18.
+ */
+
+public class ForecastAdapter {
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
