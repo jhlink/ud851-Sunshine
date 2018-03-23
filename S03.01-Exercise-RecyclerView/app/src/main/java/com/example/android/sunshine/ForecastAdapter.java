15fdcfd7b656664db9ff9baacbc36a474c285640
James Kim <jhlink@users.noreply.github.com>
2018-03-22 20:11:24 -0400

    task: CP TK [18, 21], create mWeatherTextView member and implement constructor for ForecastAdapterViewHolder

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by james on 3/22/18.
@@ -8,6 +10,12 @@ import android.support.v7.widget.RecyclerView;
 
 public class ForecastAdapter {
     public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mWeatherTextView;
 
+        public ForecastAdapterViewHolder(View inputView) {
+            super(inputView);
+            mWeatherTextView = (TextView) inputView.findViewById(R.layout
+                    .forecast_list_item);
+        }
     }
 }
