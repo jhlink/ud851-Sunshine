f2fd69ca75d82f238359285f5a59758e0d452ccc
James Kim <jhlink@users.noreply.github.com>
2018-03-22 23:03:54 -0400

    clean: Delete TDs [24, 32]

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -33,27 +33,6 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
-    // TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
-    // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
-
-    // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
-
-    // TODO (24) Override onCreateViewHolder
-    // TODO (25) Within onCreateViewHolder, inflate the list item xml into a view
-    // TODO (26) Within onCreateViewHolder, return a new ForecastAdapterViewHolder with the above view passed in as a parameter
-
-    // TODO (27) Override onBindViewHolder
-    // TODO (28) Set the text of the TextView to the weather for this list item's position
-
-    // TODO (29) Override getItemCount
-    // TODO (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
-
-    // TODO (31) Create a setWeatherData method that saves the weatherData to mWeatherData
-    // TODO (32) After you save mWeatherData, call notifyDataSetChanged
-    // Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
-
-
     // TODO (33) Delete mWeatherTextView
     private TextView mWeatherTextView;
 
