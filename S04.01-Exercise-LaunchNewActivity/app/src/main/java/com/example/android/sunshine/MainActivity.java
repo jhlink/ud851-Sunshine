57bb922d7942f2dbb36b18925ebab0a443469582
James Kim <jhlink@users.noreply.github.com>
2018-03-24 18:29:30 -0400

    task[3]: Replace Toast with launch of DetailActivityIntent with Log.v on ActivityCompletion

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,11 +16,13 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,6 +36,7 @@ import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
+import java.net.ConnectException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements ForecastAdapterOnClickHandler {
@@ -116,13 +119,22 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
      */
     @Override
     public void onClick(String weatherForDay) {
-        Context context = this;
         // COMP (1) Create a new Activity called DetailActivity using Android Studio's wizard
         // COMP (2) Change the root layout of activity_detail.xml to a FrameLayout and remove
         // unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // COMP (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Context context = MainActivity.this;
+        Class destinationActivity = DetailActivity.class;
+        Intent startDetailActivityIntent = new Intent(context, destinationActivity);
+        startActivityForResult(startDetailActivityIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            Log.v("EXPLICIT_INTENT", "SUCCESS");
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     /**
