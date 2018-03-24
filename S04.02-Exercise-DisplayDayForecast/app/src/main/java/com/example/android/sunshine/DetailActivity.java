a547797cd890e155e3a30816a958d12b2a4da585
James Kim <jhlink@users.noreply.github.com>
2018-03-24 19:41:05 -0400

    task: Add TextView in DetailActivity displaying weather data from intent extra

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,16 +2,20 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private TextView todaysWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        todaysWeatherTextView = (TextView) findViewById(R.id.tv_detail_view_todays_weather);
         String weatherData = getIntent().getStringExtra("TODAYS_WEATHER");
+        todaysWeatherTextView.setText(weatherData);
     }
 }
