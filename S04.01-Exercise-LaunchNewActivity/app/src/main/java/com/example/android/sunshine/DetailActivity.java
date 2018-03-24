b1506ee8c1e98f168f8d52699e1610cb22830454
James Kim <jhlink@users.noreply.github.com>
2018-03-24 18:59:35 -0400

    correction:FRBM: Add FORECAST_SHARE_HASHTAG to DetailActivity class

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
