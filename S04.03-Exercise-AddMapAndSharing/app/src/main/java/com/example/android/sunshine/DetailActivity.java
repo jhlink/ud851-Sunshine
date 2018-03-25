363c5ed043ebf3a3a1c7c9ad66a6868b2f233d07
James Kim <jhlink@users.noreply.github.com>
2018-03-24 20:53:51 -0400

    task: Override OptionsItemSelected method

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -34,12 +35,27 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-
         inflater.inflate(R.menu.share, menu);
 
         return true;
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+
+        switch (itemID) {
+            case 1:
+                // TODO:  FUN FUN STUFF.
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // COMP (3) Create a menu with an item with id of action_share
     // TODO (4) Display the menu and implement the forecast sharing functionality
 }
