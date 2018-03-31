76e19ebcd6849f38efc8dbdedf374e9de6a86ec9
James Kim <jhlink@users.noreply.github.com>
2018-03-30 22:19:32 -0400

    task[2]: Implement isMetric and fix getPreferredWeatherLocation to use passed context instead of 'this'

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -18,6 +18,7 @@ package com.example.android.sunshine.data;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.preference.PreferenceManager;
+import android.text.TextUtils;
 
 import com.example.android.sunshine.R;
 
@@ -94,7 +95,7 @@ public class SunshinePreferences {
         // COMP (1) Return the user's preferred location
         /** This will be implemented in a future lesson **/
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         String locationKey = context.getString(R.string.pref_location_key);
         String defaultLocation = context.getString(R.string.pref_location_default);
 
@@ -108,9 +109,18 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
+        // COMP (2) Return true if the user's preference for units is metric, false otherwise
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String unitsKey = context.getString(R.string.pref_units_key);
+        String metricKey = context.getString(R.string.pref_units_metric);
+
+        boolean isMetricResult = false;
+        if (TextUtils.equals(unitsKey, metricKey)) {
+            isMetricResult = true;
+        }
         /** This will be implemented in a future lesson **/
-        return true;
+        return isMetricResult;
     }
 
     /**
