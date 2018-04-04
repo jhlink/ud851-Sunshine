69464fe143c58f938b96c98c8b0b68b53ca425a9
James Kim <jhlink@users.noreply.github.com>
2018-04-03 20:13:49 -0400

    correction: Add activity for SunshineSyncUtils.startImmediatesync

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -99,7 +99,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             // Wipe out any potential PlacePicker latlng values so that we can use this text entry.
             SunshinePreferences.resetLocationCoordinates(activity);
             //  COMP (14) Sync the weather if the location changes
-            SunshineSyncUtils.startImmediateSync();
+            SunshineSyncUtils.startImmediateSync(activity);
         } else if (key.equals(getString(R.string.pref_units_key))) {
             // units have changed. update lists of weather entries accordingly
             activity.getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
