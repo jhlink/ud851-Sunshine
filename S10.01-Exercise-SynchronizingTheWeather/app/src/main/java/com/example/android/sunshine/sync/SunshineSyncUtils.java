fe77a09191d4e3a3476c505aef20fc133b03a983
James Kim <jhlink@users.noreply.github.com>
2018-04-03 20:05:09 -0400

    task[9-11]: Implement SunshineSyncUtils class and startImmediateSync method

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,19 @@
-// TODO (9) Create a class called SunshineSyncUtils
-    //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.android.sunshine.sync.SunshineSyncIntentService;
+
+// COMP (9) Create a class called SunshineSyncUtils
+public class SunshineSyncUtils {
+
+    //  COMP (10) Create a public static void method called startImmediateSync
+    public static void startImmediateSync(Context context) {
+        //  COMP (11) Within that method, start the SunshineSyncIntentService
+        Intent startSyncIntent = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(startSyncIntent);
+    }
+}
